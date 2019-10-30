31fcfb0db13f21225d781a882c21c234b069ddbb
savioryang <33565722+savioryang@users.noreply.github.com>
2019-10-30 16:20:01 +0800

    增加用户登录查询用户是否存在

--- WebApI/Controllers/user/UserLoginController.cs
+++ WebApI/Controllers/user/UserLoginController.cs
@@ -24,16 +24,27 @@ namespace WebApI.Controllers.user
                 je = (JObject)JsonConvert.DeserializeObject(obj.ToString());
                 datahandle.SqlConnect();
                 List<user_login> list_user = datahandle._db.Queryable<user_login>().Where(t =>
-                t.userName == je["userName"].ToString()&&t.userPwd== Md5Control.MD5Encrypt(je["userPwd"].ToString())).ToList();
-                if (list_user.Count > 0)
+               t.userName == je["userName"].ToString()).ToList();
+                if (list_user.Count ==1)
                 {
-                    jo.Add("Result", 1);
-                    jo.Add("Message", "登录成功");
+                    foreach (user_login user_Login in list_user)
+                    {
+                        if (user_Login.userPwd == Md5Control.MD5Encrypt(je["userPwd"].ToString()))
+                        {
+                            jo.Add("Result", 1);
+                            jo.Add("Message", "登录成功");
+                        }
+                        else
+                        {
+                            jo.Add("Result", 0);
+                            jo.Add("Message", "登录失败，密码错误");
+                        }
+                    }
                 }
                 else
                 {
-                    jo.Add("Result", 0);
-                    jo.Add("Message", "登录失败，密码错误");
+                    jo.Add("Result", 2);
+                    jo.Add("Message", "用户不存在，请先注册！");
                 }
             }
             catch(Exception error)
